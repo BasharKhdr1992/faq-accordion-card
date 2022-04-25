54f2c02a0c1d7abb835e900e9eb7e73c3478664f
bashar khdr <bashar.khdr.DS.92@hotmail.com>
2022-04-25 15:52:10 +0200

    adding project files

---
+++ src/assets.js
@@ -0,0 +1,11 @@
+import iwo from './assets/illustration-woman-online-desktop.svg';
+import iconArrowDown from './assets/icon-arrow-down.svg';
+import ib from './assets/illustration-box-desktop.svg';
+
+const assets = {
+  illustrationWomanOnline: iwo,
+  illustrationBox: ib,
+  iconArrowDown: iconArrowDown,
+};
+
+export default assets;
---
+++ src/components/FAQ.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react';
+import './FAQ.css';
+import assets from '../assets';
+import Divider from './UI/Divider';
+import { v4 as uuidv4 } from 'uuid';
+import { MdKeyboardArrowUp } from 'react-icons/md';
+
+const FAQS = [
+  {
+    id: uuidv4(),
+    question: 'How many team members can I invite?',
+    answer: `You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.`,
+    open: false,
+  },
+  {
+    id: uuidv4(),
+    question: 'What is the maximum file upload size?',
+    answer:
+      'No more than 2GB. All files in your account must fit your allotted storage space.',
+    open: false,
+  },
+  {
+    id: uuidv4(),
+    question: 'How do I reset my password?',
+    answer: `Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.`,
+    open: false,
+  },
+  {
+    id: uuidv4(),
+    question: 'Can I cancel my subscription?',
+    answer:
+      'Yes! Send us a message and we’ll process your request no questions asked.',
+    open: false,
+  },
+  {
+    id: uuidv4(),
+    question: 'Do you provide additional support?',
+    answer:
+      'Chat and email support is available 24/7. Phone lines are open during normal business hours.',
+    open: false,
+  },
+];
+
+const RenderFAQHeader = ({ open, question }) => {
+  if (open) {
+    return <h3 className="faq-title">{question}</h3>;
+  }
+  return <p className="faq-title">{question}</p>;
+};
+
+const FAQ = () => {
+  const [faqs, setFaqs] = useState([]);
+
+  useEffect(() => {
+    setFaqs(FAQS);
+  }, []);
+
+  const toggleFAQ = (id) => {
+    setFaqs((prev) => {
+      return prev.map((faq) => ({
+        ...faq,
+        open: faq.id === id ? !faq.open : faq.open,
+      }));
+    });
+  };
+
+  return (
+    <div className="faq">
+      <div className="faq-illustration">
+        <div className="img-wrapper">
+          <img
+            src={assets.illustrationWomanOnline}
+            id="woman-online"
+            alt="woman online"
+          />
+          <img src={assets.illustrationBox} id="box" alt="box" />
+        </div>
+      </div>
+      <div className="faq-text">
+        <h1>FAQ</h1>
+        {faqs.map((faq, index) => {
+          return (
+            <div
+              key={faq.id}
+              onClick={() => toggleFAQ(faq.id)}
+              className={`faq-wrapper ${index !== faqs.length - 1 && 'p-b'}`}
+            >
+              <div className="faq-header">
+                <RenderFAQHeader question={faq.question} open={faq.open} />
+                <div className="faq-header-icon">
+                  {faq.open ? (
+                    <MdKeyboardArrowUp color="hsl(14, 88%, 65%)" />
+                  ) : (
+                    <img src={assets.iconArrowDown} alt="arrow down icon" />
+                  )}
+                </div>
+              </div>
+              <p className={`faq-answer ${faq.open && 'faq-answer-show'}`}>
+                {faq.answer}
+              </p>
+              <Divider />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FAQ;
